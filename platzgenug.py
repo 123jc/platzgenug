6d23dae0e6c363c5bcd56454f36bfdab9b5ad9ce
Jan Cermak <jan.cermak@kit.edu>
2020-02-19 21:35:32 +0100

    Message Klimaschutzkonzept removed because no longer current

--- platzgenug.py
+++ platzgenug.py
@@ -17,7 +17,6 @@ vergleichsFlaechen = {'Marktplatz':6000,'Friedrichsplatz':12100,'Botanischer Gar
 alternativeUseAreas = {'Bäume': parkplatzFlaeche/1.1, 'Cafétische oder Ruhebänke':parkplatzFlaeche/1.5, 'Kinderwippen':parkplatzFlaeche/.9 ,'Fahrradbügelstellplätze':parkplatzFlaeche/9.}  # aus Parking Day-Fotos geschaetzt
 statements = ["Wo sind die überdachten Fahrradbügel?",
               "Karlsruhe: Roter Teppich für Autos.",
-              "Jetzt das städtische Klimaschutzkonzept kommentieren: https://beteiligung.karlsruhe.de/content/bbv/details/90/",
               "Sind trotzdem noch Radwege zugeparkt?",
               "Sind dennoch weiter Autos im Halteverbot abgestellt?",
               "Fahren dennoch weiter Autos auf der Suche nach kostenlosen Parkplätzen durch die Stadt?"]
